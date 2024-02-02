fe4b346523aa4937854a02aace6e740d694a1f04
hemanthio <121523327+hemanthio@users.noreply.github.com>
2024-02-02 19:47:01 +0530

    schema

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
-const URI = "mongodb://127.0.0.1:27017/mern_admin";
+
+// const URI = "mongodb://127.0.0.1:27017/mern_admin";
+const URI =process.env.MONGODB_URI;
 
 // mongoose.connect()
 
 const connectDb = async ()=>{
+
     
     try {
         await mongoose.connect(URI)
