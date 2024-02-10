2d1adac32941257fdb23bb07c490931e569bc801
hemanthio <121523327+hemanthio@users.noreply.github.com>
2024-02-11 00:30:13 +0530

    axios

--- apnaCollege/tutorial25/api.js
+++ apnaCollege/tutorial25/api.js
@@ -1,13 +1,26 @@
-let url = "https://catfact.ninja/facts"
+const axios = require('axios');
 
-fetch(url)
-.then((res)=>{
-    console.log(res);
-    return res.json()
-})
-.then((data)=>{
-    console.log(data.data)
-})
-.catch((err)=>{
-    console.log("error caught",err)
-})
+// let url = "https://catfact.ninja/facts"
+
+// async function getFacts(){
+//     try {
+//         let res = await fetch(url);
+//         let data = await res.json();
+//         console.log(data.fact)
+//     } catch (error) {
+//         console.log("error occured",error)
+//     }
+// }
+
+
+
+//using axios
+async function factjoke(){
+    try {
+        let res = await axios.get("https://catfact.ninja/fact");
+        console.log(res)
+    } catch (error) {
+        console.log(error);
+    }
+}
+factjoke()
