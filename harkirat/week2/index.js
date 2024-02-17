a2731d9f333d9e8eedd2d6ef55879d889108a376
hemanthio <121523327+hemanthio@users.noreply.github.com>
2024-02-17 16:35:36 +0530

    get and post

--- harkirat/week2/index.js
+++ harkirat/week2/index.js
@@ -12,10 +12,12 @@ function calculate(counter) {
 }
 
 
-// app.get('/', (req, res) => {
-//     const sumOfIntegers = calculate(100)
-//   res.send(`sum of numbers is ${sumOfIntegers}`)
-// })
+app.get('/', (req, res) => {
+    const sumOfIntegers = calculate(100)
+  res.send(`sum of numbers is ${sumOfIntegers}`)
+})
+
+
 app.get('/handlesum', (req, res) => {
     var counter = req.query.counter
     const handlesumof =calculate(counter)
@@ -23,6 +25,36 @@ app.get('/handlesum', (req, res) => {
    
   res.send(`hello world namste how are you`)
 })
+
+// app.get('/:username', (req, res) => {
+//   var username = req.params.username
+ 
+//   res.send(`hello ${username}`)
+ 
+
+// })
+
+app.get('/username', (req, res) => {
+  var username= req.query.username
+ 
+  res.send(`hello ${username}`)
+ 
+
+})
+
+app.post("/handle",(req,res)=>{
+  res.send("hello word")
+})
+app.put("/handler",(req,res)=>{
+  res.send("hello word")
+})
+
+app.delete("/handless",(req,res)=>{
+  res.send("hello word")
+})
+
+
+
 
 
 app.listen(port, () => {
